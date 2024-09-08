7a0da1378c6d94693013549902d45b850fdf1909
Arjun Porwal <porwalarjun95@gmail.com>
2024-09-08 17:24:35 +0530

    feat: created campaign card

--- client/components/landing/index.tsx
+++ client/components/landing/index.tsx
@@ -1,7 +1,14 @@
+import { Campaign } from "./campaign";
+
 export const Landing = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl mt-8">All Campaigns</h1>
+      <h1 className="text-2xl my-8">All Campaigns</h1>
+      <div className="grid grid-cols-4 gap-6">
+        {Array.from({ length: 5 }).map((_, i) => {
+          return <Campaign key={i} />;
+        })}
+      </div>
     </div>
   );
 };
--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,6 +1,13 @@
+import moment from "moment";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const daysLeft = (timestamp: number) => {
+  const eventdate = moment(timestamp * 1000);
+  const todaysdate = moment();
+  return eventdate.diff(todaysdate, "days");
+};
