6ea6168bc5426e6f9a295d3cc90e9d1e030ff2cc
Arjun Porwal <porwalarjun95@gmail.com>
2024-10-13 13:11:03 +0530

    chore minor change

--- client/components/landing/index.tsx
+++ client/components/landing/index.tsx
@@ -1,13 +1,16 @@
 import { getAllDeployedCampaigns } from "@/fetchers";
 
 import { Campaign } from "./campaign";
+import { Telescope } from "lucide-react";
 
 export const Landing = async () => {
   const campaigns = await getAllDeployedCampaigns();
 
   return (
     <div className="space-y-10 py-12">
-      <h1 className="text-2xl font-medium">All Campaigns</h1>
+      <h1 className="flex items-center gap-2 text-2xl font-medium text-primary">
+        <Telescope /> Discover all active campaigns
+      </h1>
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {campaigns.map((campaign) => {
           return <Campaign campaign={campaign} key={campaign.address} />;
