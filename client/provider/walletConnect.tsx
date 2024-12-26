02df5d54338f50570ae0e870ae7a19ef98b621b6
Arjun Porwal <porwalarjun95@gmail.com>
2024-12-26 16:29:43 +0530

    chore: remove redundant changes

--- client/provider/walletConnect.tsx
+++ client/provider/walletConnect.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ReactNode, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
 
 import {
@@ -9,7 +8,6 @@ import {
   ThemeMode,
   createAppKit,
   useAppKitTheme,
-  useAppKitAccount,
 } from "@reown/appkit/react";
 import { EthersAdapter } from "@reown/appkit-adapter-ethers";
 import { AppKitNetwork, hardhat, sepolia } from "@reown/appkit/networks";
@@ -53,10 +51,7 @@ export const WalletConnectProvider = ({
 }: {
   children: ReactNode;
 }) => {
-  const router = useRouter();
-
   const { theme } = useTheme();
-  const { address } = useAppKitAccount();
   const { setThemeMode, setThemeVariables } = useAppKitTheme();
 
   useEffect(() => {
@@ -66,10 +61,6 @@ export const WalletConnectProvider = ({
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [theme]);
-
-  // Refresh server components on account change
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => router.refresh(), [address]);
 
   return <>{children}</>;
 };
