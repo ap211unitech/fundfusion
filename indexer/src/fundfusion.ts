816f8a4320ecb9b240bd3a808a8d2bdbcf1c799d
Arjun Porwal <porwalarjun95@gmail.com>
2025-03-09 00:20:11 +0530

    feat: add FundFusion contract ABI and event handling for campaign creation

---
+++ indexer/src/fundfusion.ts
@@ -0,0 +1,15 @@
+import { NewCampaignCreated as NewCampaignCreatedEvent } from "../generated/FundFusion/FundFusion";
+import { DeployedCampaign } from "../generated/schema";
+
+export function handleNewCampaignCreated(event: NewCampaignCreatedEvent): void {
+  const id = event.transaction.hash
+    .concatI32(event.logIndex.toI32())
+    .toString();
+  const entity = new DeployedCampaign(id);
+
+  entity.owner = event.params.creator;
+  entity.campaign = event.params.campaign;
+  entity.createdAt = event.params.timestamp;
+
+  entity.save();
+}
