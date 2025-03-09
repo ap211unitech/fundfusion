6924bfda207608beefd53552d6639d8d87f36652
Arjun Porwal <porwalarjun95@gmail.com>
2025-03-09 10:33:57 +0530

    fix: change id type from String to Bytes in DeployedCampaign and optimize ID generation in event handler

--- indexer/src/fundfusion.ts
+++ indexer/src/fundfusion.ts
@@ -2,9 +2,7 @@ import { NewCampaignCreated as NewCampaignCreatedEvent } from "../generated/Fund
 import { DeployedCampaign } from "../generated/schema";
 
 export function handleNewCampaignCreated(event: NewCampaignCreatedEvent): void {
-  const id = event.transaction.hash
-    .concatI32(event.logIndex.toI32())
-    .toString();
+  const id = event.transaction.hash.concatI32(event.logIndex.toI32());
   const entity = new DeployedCampaign(id);
 
   entity.owner = event.params.creator;
