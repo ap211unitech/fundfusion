be7b51a3fdfafdcc39b082d0032706b22f20dae9
Arjun Porwal <porwalarjun95@gmail.com>
2025-03-10 22:28:00 +0530

    feat: implement handleFundDonated function

--- indexer/src/campaign.ts
+++ indexer/src/campaign.ts
@@ -1,3 +1,15 @@
 import { FundDonated as FundDonatedEvent } from "../generated/templates/Campaign/Campaign";
+import { Contributor } from "../generated/schema";
 
-export function handleFundDonated(event: FundDonatedEvent): void {}
+export function handleFundDonated(event: FundDonatedEvent): void {
+  const id = event.transaction.hash.concatI32(event.logIndex.toI32());
+  const entity = new Contributor(id);
+
+  entity.contributor = event.params.contributor;
+  entity.campaign = event.params.campaign;
+  entity.amount = event.params.amount;
+  entity.timestamp = event.params.timestamp;
+  entity.hasClaimedRefund = false;
+
+  entity.save();
+}
